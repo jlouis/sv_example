74e9e885ace65f6e9f209c24a7e9124214bbca0e
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2013-09-24 10:41:30 +0200

    Introduce a variant which does a better job at using REST correctly.

--- sv_example/src/sv_example_handler.erl
+++ sv_example/src/sv_example_handler.erl
@@ -6,11 +6,17 @@
 -export([allowed_methods/2,
          content_types_provided/2,
          get_json/2,
-         init/3,
-         terminate/3]).
+         init/3, rest_init/2,
+         terminate/3, rest_terminate/2]).
 
 init({tcp, http}, _Req, _Opts) ->
     {upgrade, protocol, cowboy_rest}.
+
+rest_init(Req, _State) ->
+    case sv:ask(web_queue, sv:timestamp()) of
+        {go, Ref} -> {ok, Req, {go, Ref}};
+        {error, Reason} -> {ok, Req, {error, Reason}}
+    end.
 
 allowed_methods(Req, State) ->
     {[<<"GET">>], Req, State}.
@@ -18,20 +24,19 @@ allowed_methods(Req, State) ->
 content_types_provided(Req, State) ->
     {[{{<<"application">>, <<"json">>, []}, get_json}], Req, State}.
 
-get_json(Req, State) ->
-    case sv:run(web_queue, fun () -> xkcd_random() end) of %% Ask SV // HL
-        {ok, RandomNum} ->
-            RespBody = [{<<"Method">>, <<"XKCD-221 RNG">>},
-                        {<<"Result">>, RandomNum}],
-            {jsx:to_json(RespBody), Req, State};
-        {error, Reason} ->
-            R = atom_to_binary(Reason, utf8),
-            {ok, Reply} =
-              cowboy_req:reply(503, [],
-                               <<"System Overloaded ", R/binary>>, Req),
-            {halt, Reply, State}
-    end.
+get_json(Req, {error, Reason}) ->
+    R = atom_to_binary(Reason, utf8),
+    {ok, Reply} = cowboy_req:reply(503, [], <<"System Overloaded ", R/binary>>, Req),
+    {halt, Reply, {error, Reason}};
+get_json(Req, {go, _Ref} = State) ->
+    RandomNum = xkcd_random(),
+    RespBody = [{<<"Method">>, <<"XKCD-221 RNG">>},
+                {<<"Result">>, RandomNum}],
+    {jsx:to_json(RespBody), Req, State}.
 
+rest_terminate(_Req, {error, _Reason}) -> ok;
+rest_terminate(_Req, {go, Ref}) -> sv:done(web_queue, Ref, sv:timestamp()), ok.
+    
 terminate(_Reason, _Req, _State) ->
     ok.
 
